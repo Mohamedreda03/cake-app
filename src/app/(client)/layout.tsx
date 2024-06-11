cee1109e5275cb054e3c8643dcbd4df693d4babd
Mohamedreda03 <mo03.reda@gmail.com>
2024-06-11 16:31:13 +0300

    fffffff

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -1,5 +1,7 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/navbar/Navbar";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
@@ -7,6 +9,18 @@ export default function layout({ children }: { children: React.ReactNode }) {
     <div className="bg-color-3/20">
       <Navbar />
       <main>{children}</main>
+      <Link
+        href="tel:+966555702562"
+        className="fixed md:right-5 md:bottom-5 right-4 bottom-4"
+      >
+        <Image
+          src="/icons/whatsapp.svg"
+          alt="logo"
+          width={150}
+          height={150}
+          className="h-14 w-14 md:h-16 md:w-16 p-3 rounded-full bg-green-500 hover:bg-green-600 transition-all cursor-pointer"
+        />
+      </Link>
       <Footer />
     </div>
   );
