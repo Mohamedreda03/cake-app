5ebf36b36289709852eaf648edb13b8ae0f58aa2
Mohamedreda03 <mo03.reda@gmail.com>
2024-06-13 13:36:40 +0300

    ...

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,9 +3,6 @@ import {MetadataRoute} from "next"
 export default async function sitemap():Promise<MetadataRoute.Sitemap> {
     return [
     {
-        url: `${process.env.NEXT_PUBLIC_URL}`,
-        lastModified: new Date(),
-    },{
         url: `${process.env.NEXT_PUBLIC_URL}/about`,
         lastModified: new Date(),
     },{
