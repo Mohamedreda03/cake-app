364cd8e0d35359ff570750663a406d16308c8b08
Mohamed Reda <mo03.reda@gmail.com>
2024-06-20 07:57:23 +0300

    payment edit

---
+++ src/app/test/page.tsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+export default function Test() {
+  const testWebhook = async () => {
+    await axios
+      .request({
+        url: "https://api.moyasar.com/v1/webhooks",
+        method: "POST",
+        auth: {
+          username: process.env.NEXT_PUBLIC_MOYASAR_API_KEY!,
+          password: "",
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          http_method: "POST",
+          url: process.env.NEXT_PUBLIC_URL,
+          shared_secret: "test",
+          events: ["payment_paid"],
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+  return <div>Test</div>;
+}
