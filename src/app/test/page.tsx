b1c7872d656ef74e78a97ef348e69ca60c9fe07e
Mohamed Reda <mo03.reda@gmail.com>
2024-06-20 08:12:11 +0300

    payment edit

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,6 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import axios from "axios";
+import { useEffect } from "react";
 
 export default function Test() {
   const testWebhook = async () => {
@@ -27,6 +28,26 @@ export default function Test() {
         console.log(res.data);
       });
   };
+
+  useEffect(() => {
+    const te = async () => {
+      await axios
+        .request({
+          url: "https://api.moyasar.com/v1/webhooks",
+          method: "GET",
+          auth: {
+            username: process.env.NEXT_PUBLIC_MOYASAR_API_KEY!,
+            password: "",
+          },
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+        });
+    };
+  }, []);
   return (
     <div>
       <Button onClick={testWebhook}>Test Webhook</Button>
