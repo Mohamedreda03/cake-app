550261fbd109b6a64d0d223e21a86aff834fc15c
Mohamed Reda <mo03.reda@gmail.com>
2024-06-20 10:25:10 +0300

    seo edit

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from "next";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {
-      url: `${process.env.NEXT_PUBLIC_URL}/`,
+      url: `${process.env.NEXT_PUBLIC_URL}`,
       lastModified: new Date(),
     },
     {
-      url: `${process.env.NEXT_PUBLIC_URL}/about`,
+      url: `${process.env.NEXT_PUBLIC_URL}about`,
       lastModified: new Date(),
     },
     {
-      url: `${process.env.NEXT_PUBLIC_URL}/menu`,
+      url: `${process.env.NEXT_PUBLIC_URL}menu`,
       lastModified: new Date(),
     },
     {
-      url: `${process.env.NEXT_PUBLIC_URL}/cart`,
+      url: `${process.env.NEXT_PUBLIC_URL}cart`,
       lastModified: new Date(),
     },
   ];
