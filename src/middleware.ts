b2e6322e9dc896382b4a72d5a27cd76aefc69a40
Mohamedreda03 <mo03.reda@gmail.com>
2024-07-19 23:42:45 +0300

    c

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,14 +14,13 @@ const publicPages = [
   "/cart",
 ];
 
-// const authMiddleware =
-export default auth((req) => {
-  const intlMiddleware = createMiddleware({
-    locales,
-    defaultLocale: "ar",
-    localeDetection: false,
-  });
+const intlMiddleware = createMiddleware({
+  locales,
+  defaultLocale: "ar",
+  localeDetection: false,
+});
 
+const authMiddleware = auth((req) => {
   if (req.nextUrl.pathname.includes("/dashboard") && !req.auth) {
     return NextResponse.redirect(new URL("/", req.nextUrl).toString());
   }
@@ -48,20 +47,20 @@ export default auth((req) => {
   return intlMiddleware(req);
 });
 
-// export default async function middleware(req: NextRequest) {
-//   const publicPathnameRegex = RegExp(
-//     `^(/(${locales.join("|")}))?(${publicPages
-//       .flatMap((p) => (p === "/" ? ["", "/"] : p))
-//       .join("|")})/?$`,
-//     "i"
-//   );
-//   const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname);
-//   if (isPublicPage) {
-//     return intlMiddleware(req);
-//   } else {
-//     return (authMiddleware as any)(req);
-//   }
-// }
+export default async function middleware(req: NextRequest) {
+  const publicPathnameRegex = RegExp(
+    `^(/(${locales.join("|")}))?(${publicPages
+      .flatMap((p) => (p === "/" ? ["", "/"] : p))
+      .join("|")})/?$`,
+    "i"
+  );
+  const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname);
+  if (isPublicPage) {
+    return intlMiddleware(req);
+  } else {
+    return (authMiddleware as any)(req);
+  }
+}
 
 export const config = {
   matcher: [
